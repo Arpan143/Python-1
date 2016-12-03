e77abce6747d4f5860b9ecfc425e469ac36abb50
HyWell <hywell.28@gmail.com>
2016-12-03 23:00:39 +0800

    修复无法打开href问题，更改编码（现为ascii）

--- Os/File_html_Windows.py
+++ Os/File_html_Windows.py
@@ -3,38 +3,38 @@ import time,os,regex,re,sys
 
 class File_html_Windows():
     def __init__(self):
-        self.html = u"""
-<self.html>
+        self.html = u"""<html>
 <head>
 <title>Directories</title>
-<meta http-equiv="Content-Type" content="text/self.html;charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 </head>
 <body>
 <h1>Directories</h1>
 <ul>
 """
     def Windows_all(self):
-        path = os.getcwd()
-        path = raw_input('ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¿Â¼(C:\Test\Taeget_folder):')
+        # path = os.getcwd()
+        path = raw_input('ÇëÊäÈëÄ¿Â¼C:\Test\Taeget_folder):')
+        FilePath = path + '/Ä¿Â¼.html'
         path = unicode(path,sys.stdin.encoding)
         a = 1
         for root, dirs, files in os.walk(path):
             if root[len(path):] == '':
-                self.html = self.html + '<details>\n<summary>ï¿½ï¿½Ä¿Â¼</summary>'.decode(sys.stdin.encoding)
+                self.html = self.html + '<details>\n<summary>¸ùÄ¿Â¼</summary>'.decode(sys.stdin.encoding)
                 for y in files:
-                    self.html = self.html + '\n<li><a href = >' + y + '</a></li>'
+                    self.html = self.html + '\n<li><a href = "' + root + '\\' + y +'">' + y + '</a></li>'
                 self.html = self.html +'\n</details>'
             else:
                 self.html = self.html + '<details>\n<summary>' + root[len(path)+1:] +'</summary>'
                 for y in files:
-                    self.html = self.html + '\n<li><a href = >' + y + '</a></li>'
+                    self.html = self.html + '\n<li><a href = "' + root + '\\' + y +'">' + y + '</a></li>'
                 self.html = self.html +'\n</details>'
             a =a +1
             print a
         self.html = self.html + '</ul>\n</body>\n</self.html>'
         self.html =self.html.encode('utf-8')
         # self.html = self.html.decode(sys.stdin.encoding).encode('utf-8')
-        f = open('test.html','w+')
+        f = open(FilePath,'w+')
         f.write(self.html)
         f.close()
 
