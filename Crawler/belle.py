6521ba551b9f8e93165c4bd69063ce5b30148a81
HyWell <hywell.28@gmail.com>
2016-12-14 22:00:41 +0800

    使用队列、进程池下载图片

--- Crawler/belle.py
+++ Crawler/belle.py
@@ -2,39 +2,57 @@
 
 from bs4 import BeautifulSoup
 import requests
-import threading
+from multiprocessing import Process,Queue,Pool
+import multiprocessing
 import time
-import os
+import random
 
 
-def save_img(url_str):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
-               }
-    soup = BeautifulSoup(requests.get('https://belle.la/page_' + str(url_str) + '/', headers=headers).text, 'lxml')
-    os.mkdir(r'./Download')
-    for y in soup.find_all('img'):
-        img_url = y.get('src')
-        img_name = y.get('alt')
-        if type(img_name) == unicode:
-            img_name = img_name.encode('gbk')
+def read_img_url(q):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)' +
+                             ' Chrome/55.0.2883.75 Safari/537.36'}
+    for i in range(100):
+        soup = BeautifulSoup(requests.get('https://belle.la/page_' + str(i) + '/', headers=headers).text, 'lxml')
+        for y in soup.find_all('img'):
+            img_url = y.get('src')
+            img_name = y.get('alt')
+            if type(img_name) == unicode:
+                try:
+                    img_name = img_name.encode('gbk')
+                except:
+                    print img_name
+                    img_name = img_name.encode('utf-8')
+            else:
+                pass
+            q.put([img_name, img_url], block=False)
+    q.put('Pleace End!')
+
+
+def save_img(q):
+    print 'Going!'
+    while True:
+        if not q.empty():
+            img_list = q.get(False)
+            if img_list == 'Pleace End!':
+                break
+            else:
+                with open(r'.\Download\{}.{}'.format(img_list[0], img_list[1][len(img_list[1])-3: len(img_list[1])]), 'wb+') as img_hex:
+                        img_hex.write(requests.get(img_list[1]).content)
+                time.sleep(random.random())
         else:
             pass
-        print img_url
-        with open(r'.\Download\{}.{}'.format(img_name, img_url[len(img_url)-3: len(img_url)]), 'wb+') as img_hex:
-            img_hex.write(requests.get(img_url).content)
-            time.sleep(2)
+    print "Ending!"
 
 
 def main():
-    threads = []
-    saves = 20
-    for i in range(saves):
-        t = threading.Thread(target=save_img, args=[i])
-        threads.append(t)
-    for i in range(saves):
-        threads[i].start()
-    for i in range(saves):
-        threads[i].join()
+    manager = multiprocessing.Manager()
+    q = manager.Queue()
+    # lock = manager.Lock()
+    read_img_url(q)
+    p = Pool(processes=20)
+    pr = p.apply_async(save_img, (q,))
+    p.close()
+    p.join()
 
 if __name__ == '__main__':
     main()
