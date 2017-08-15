ee44d1db6dd09863a7f8d282f496f2972760b5ba
hywell <hywell.28@gmail.com>
2017-08-15 16:16:22 +0800

    现支持生成公钥文件,私钥文件、RSA加密、RSA解密 RSA加密需要指定公钥文件、加密目录 RSA解密需要指定私钥文件、解密迷路

--- Os/Encryptiong.py
+++ Os/Encryptiong.py
@@ -1,99 +1,151 @@
 #!/usr/bin/env python
 # coding=utf-8
 import os
+import getopt
 import sys
-import platform
 import binascii
-# import random
+from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+from Crypto.PublicKey import RSA
+import tqdm
 
 
 class Encryptiong():
-    def __init__(self, hex_str):
-        # self.random_str = random.random()
-        self.hex_str = hex_str
-        self.enc_str = ''
-        self.encryptiong_dict = {
-                '0': 'f',
-                '1': 'e',
-                '2': 'd',
-                '3': 'c',
-                '4': 'b',
-                '5': 'a',
-                '6': '9',
-                '7': '8',
-                '8': '7',
-                '9': '6',
-                'a': '5',
-                'b': '4',
-                'c': '3',
-                'd': '2',
-                'e': '1',
-                'f': '0',
-            }
+    def __init__(self, generate=None, public=None, filepath=None, decrypt=None, private=None, generateNum=None):
+        self.generate = generate
+        self.filepath = filepath
+        self.decrypt = decrypt
+        self.private = private
+        self.public = public
+        if generateNum:
+            self.generateNum = int(generateNum)
+        else:
+            self.generateNum = 1024
 
-    def encryptiong(self):
-        # salt = self.random[2]
-        for i in self.hex_str:
-            if i in self.encryptiong_dict:
-                self.enc_str += self.encryptiong_dict[i]
-            else:
-                self.enc_str += i
-        return binascii.a2b_hex(self.enc_str)
-        
+    def encryp(self):
+        random_generator = Random.new().read
+        rsa = RSA.generate(self.generateNum, random_generator)
+        private_pem = rsa.exportKey()
+        with open('private.pem', 'w') as f:
+            f.write(private_pem)
+        public_pem = rsa.publickey().exportKey()
+        with open('public.pem', 'w') as f:
+            f.write(public_pem)
 
-class Work_NT(): ##Windows
-    def __init__(self, path_dir, python_version): ##定义所需变量
-        self.python_exe = os.popen('where python', 'r').read().replace('\n','')
-        self.python_dir = os.path.dirname(self.python_exe)
-        self.dir = path_dir
-        self.python_version = python_version
-        self.get_file_list = []
-        self.get_pyfile_list = []
-        
-    def get_all_info(self): ##获取指定目录下所有文äﾻ?
-        for root,dirs,files in os.walk(self.dir):
+    def encryption(self):
+        res = []
+        length = 100
+        with open(self.public) as f:
+            key = f.read()
+        rsakey = RSA.importKey(key)
+        cipher = Cipher_pkcs1_v1_5.new(rsakey)
+        result = self.getinfo(self.filepath)
+        while True:
+            try:
+                file_name, name, cipher_text = result.next()
+                for i in tqdm.tqdm(range(0, len(cipher_text), length), desc=name + " is being encrypted:"):
+                    res.append(cipher.encrypt(binascii.b2a_hex(cipher_text)[i:i+length]))
+                with open(file_name, 'wb') as f:
+                    f.write("".join(res))
+                    res = []
+            except StopIteration:
+                break
+
+    def decrypted(self):
+        res = []
+        length = 128
+        with open(self.private) as f:
+            key = f.read()
+        rsakey = RSA.importKey(key)
+        cipher = Cipher_pkcs1_v1_5.new(rsakey)
+        random_generator = Random.new().read
+        result = self.getinfo(self.decrypt)
+        while True:
+            try:
+                file_name, name, cipher_text = result.next()
+                for i in tqdm.tqdm(range(0, len(cipher_text), length), desc=name + " is being decrypted:"):
+                        res.append(cipher.decrypt(cipher_text[i:i+length], random_generator))
+                with open(file_name, 'wb') as f:
+                    f.write(binascii.a2b_hex("".join(res)))
+                    res = []
+            except StopIteration:
+                break
+
+    def getinfo(self, filepath):
+        get_file_list = []
+        for root, dirs, files in os.walk(filepath):
             for name in files:
-                self.get_file_list.append(os.path.join(root,name))
+                get_file_list.append(os.path.join(root, name))
+                with open(os.path.join(root, name), 'rb') as f:
+                    yield [os.path.join(root, name), name, f.read()]
 
-    def get_file_type(self): ##判断文件类型,现阶段使用后缀判断.后期修改成通过文件头判æﾖ?
-        for i in self.get_file_list:
-            if os.path.splitext(i)[1] == '.dll':
-                self.get_pyfile_list.append(i)
-        # for i in self.get_file_list:
-            # with open(i, 'rb+')
-
-    def work(self): ##向指定文件类型进行写入操äﾽ?现阶段为向py类型写入
-        for i in self.get_pyfile_list:
-            with open(i, 'rb+') as f:
-                hex_str = f.read().encode('hex')
-            with open(i, 'wb') as f:
-                enc_work = Encryptiong(hex_str)
-                f.write(enc_work.encryptiong())
-#                content = f.read()
-#               f.seek(0, 0)
-#              f.write('print "Fuck"\n' + content)
-
-    def go_die(self):
-        os.remove(os.getcwd() + '\\' + os.path.basename(__file__))
 
 def main():
-    result = raw_input("Dont to go!")
-    if result == "Just Go":
-        os_version = platform.system()
-        python_version = sys.version
-        if sys.version[0] == '2':
-            python_version = 2
+    print """
+     _        _   _      _  _    __    _  __________  _           _
+    | |      | | \ \    / /| |  /  \  | ||  ________|| |         | |
+    | |______| |  \ \  / / | | / /\ \ | || |________ | |         | |
+    |  ______  |   \ \/ /  | | | || | | ||  ________|| |         | |
+    | |      | |    \  /   | |_| || |_| || |         | |         | |
+    | |      | |    |  |    \   /  \   / | |________ | |________ | |________
+    |_|      |_|    |__|     \_/    \_/  |__________||__________||__________|
+
+        """
+    try:
+        encryptor = None  # 生成公、秘钥的开关
+        generate = False  # 加密功能的开关
+        decrypt = False  # 解密功能的开关
+        keyfile = None  # 公钥或密钥文件路径
+        file = None  # 加密或解密文件目录
+        opts, args = getopt.getopt(sys.argv[1:], "-h-e-g-d-k:-f:",
+                                   ["help", "encryptor", "generate", "decrypt", "keyfile=", "file="])
+        for opt_name, opt_value in opts:
+            if opt_name in ('-h', '--help'):
+                print "python encrypto.py -k=D:\\private.pem(private path)"
+                exit()
+            if opt_name in ('-e', '--encryptor'):
+                encryptor = True
+            if opt_name in ('-g', '--generate'):
+                generate = True
+            if opt_name in ('-d', '--decrypt'):
+                decrypt = True
+            if opt_name in ('-k', '--keyfile'):
+                keyfile = opt_value[1:]
+            if opt_name in ('-f', '--file'):
+                file = opt_value[1:]
+        if encryptor:  # 生成公、密钥功能
+            Encryptiong(generate=True).encryp()
+        elif generate:  # 加密功能
+            if file and keyfile:
+                Encryptiong(public=keyfile, filepath=file).encryption()
+            elif file is None:
+                print 'Place input encrypto file path!'
+                print 'Example:python Encryptiong.py -g -f=D:\\encryptoFilePath\\ -k=D:\\public.pem'
+                exit()
+            elif keyfile is None:
+                print 'Place input public file path!'
+                print 'Example:python Encryptiong.py -g -f=D:\\encryptoFilePath\\ -k=D:\\public.pem'
+                exit()
+        elif decrypt:  # 解密功能
+            if keyfile and file:
+                Encryptiong(decrypt=file, private=keyfile).decrypted()
+            elif file is None:
+                print 'Place input decrypt file path!'
+                print 'Example:python Encryptiong.py -d -r=D:\\decryptedFilPath\\ -k=D:\\private.pem'
+                exit()
+            elif keyfile is None:
+                print 'Place input private file path!'
+                print 'Example:python Encryptiong.py -d -r=D:\\decryptedFilePath\\ -k=D:\\private.pem'
+                exit()
         else:
-            python_version = 3
-        python_dir = r'D:\1'
-        if os_version == 'Windows':
-            Windows_work = Work_NT(python_dir, python_version)
-            Windows_work.get_all_info()
-            Windows_work.get_file_type()
-            Windows_work.work()
-            # Windows_work.go_die()
-        elif os_version == 'Linux':
-            pass
+            print 'Example1:python Encryptiong.py -e'
+            print 'Example2:python Encryptiong.py -g -f=D:\\encryptoFilePath\\ -k=D:\\public.pem'
+            print 'Example3:python Encryptiong.py -d -f=D:\\decryptedFilePath\\ -k=D:\\private.pem'
+
+    except getopt.GetoptError:
+        print 'Example1:python Encryptiong.py -e'
+        print 'Example2:python Encryptiong.py -g -f=D:\\encryptoFilePath\\ -k=D:\\public.pem'
+        print 'Example3:python Encryptiong.py -d -f=D:\\decryptedFilePath\\ -k=D:\\private.pem'
 
 if __name__ == '__main__':
     main()
