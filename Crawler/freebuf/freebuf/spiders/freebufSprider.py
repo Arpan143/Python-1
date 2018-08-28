e2a1acc215936ad663359ead0dd8b4ce590c7bed
hywell <hywell.28@gmail.com>
2018-08-28 15:30:02 +0800

    scrapy学习
    使用scrapy爬取freebuf站点资讯

---
+++ Crawler/freebuf/freebuf/spiders/freebufSprider.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from freebuf.items import FreebufItem
+from scrapy import Request
+
+
+class FreebufspriderSpider(scrapy.Spider):
+    name = 'freebufSprider'
+    allowed_domains = ['freebuf.com']
+    start_urls = ['http://www.freebuf.com']
+    cookie = {
+        'acw_sc__': '5b84f02e3f202def131a1c138b0a017b48a97e2f',
+    }
+
+    def start_requests(self):
+        yield Request(self.start_urls[0], callback=self.parse, cookies=self.cookie)
+
+    def parse(self, response):
+        # title = response.css('title::text').extract_first()
+        for i in response.xpath("//div[@class='news_inner news-list']/div[@class='news-info']/dl"):
+            yield {
+                'title': i.xpath('dt/a/@title').extract_first(),
+                'href': i.xpath('dt/a/@href').extract_first(),
+                'author': i.xpath('dd/span[@class="name-head"]/a/@title').extract_first(),
+                'time': i.xpath('dd/span[@class="time"]/text()').extract_first().strip(),
+                'description': i.xpath('dd[@class="text"]/text()').extract_first().strip(),
+            }
